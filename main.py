9de6a278a4688fa72b9d9aa3246c3b52641e0c46
Luiz-Eller <luizeduardoeller99@gmail.com>
2022-12-01 11:40:31 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+"""
+Dawkins’ Weasel Program
+Activity 001 - Class of LPC 2022/2 - UEA/EST
+Author: Luiz Eduardo Eller
+Last Modified on: 01/12/2022
+"""
+
+
+import random
+
+# Declare Constants
+USE_DEFAULT_MESSAGE = False
+LETTER_MUTATION_PERCENTAGE = 5
+NUMBER_OF_CLONES = 100
+
+
+# Generates and returns a character that can be any
+# lower or uppercase letters or an empty space
+# checks to see if the char is not the excluded char
+def random_char(excluded_char=''):
+    char = excluded_char
+    while char == excluded_char:
+        offset = random.randrange(0, 53)
+        if offset == 0:
+            char = ' '
+
+        elif 0 < offset <= 26:
+            # Returns a uppercase letter
+            char = chr(65 + offset - 1)
+
+        else:
+            # Returns a lowercase letter
+            char = chr(97 + offset - 27)
+    return char
+
+
+def give_score(desired_string, current_string):
+    # Returns the number of letter that match (same letter and position)
+    # between two strings
+    # Is case-sensitive
+    score = 0
+    goal = list(desired_string)
+    current = list(current_string)
+    for ite in range(len(goal)):
+        if goal[ite] == current[ite]:
+            score += 1
+    return score
+
+
+def mutate_string(text):
+    # Returns a new string with a chance of some characters
+    # to swapped with a new random one
+    mutated_text = list(text)
+    for ite in range(len(mutated_text)):
+        if random.randint(0, 100) < LETTER_MUTATION_PERCENTAGE:
+            mutated_text[ite] = random_char(mutated_text[ite])
+    return "".join(mutated_text)
+
+
+# Initializes variables
+if USE_DEFAULT_MESSAGE:
+    desired_message = "METHINKS IT IS LIKE A WEASEL"
+else:
+    desired_message = input()
+message_length = len(desired_message)
+matriz_string = []
+highest_score = 0
+highest_score_position = 0
+generation = 0
+
+
+for i in range(NUMBER_OF_CLONES):
+    # Initializes the strings, all with random characters
+    # Only creates strings of the same length as the desired message
+    temp_string = ''
+    for j in range(message_length):
+        temp_string += random_char()
+    matriz_string.append(temp_string)
+
+while highest_score < message_length:
+    highest_score = 0
+    for i in range(NUMBER_OF_CLONES):
+        # Checks the highest score, if tied chooses the first one to appear
+        temp_score = give_score(desired_message, matriz_string[i])
+        if temp_score > highest_score:
+            highest_score_position = i
+            highest_score = temp_score
+
+    winner_string = matriz_string[highest_score_position]
+    print(f"Winner of generation {generation}: {winner_string}"
+          f" Score: {highest_score}")
+    generation += 1
+
+    for i in range(NUMBER_OF_CLONES):
+        matriz_string[i] = mutate_string(winner_string)
